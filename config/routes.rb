6e74ed9aadca37ba4a3c8f218682c8ac7e0e5af8
caveman07 <ram07kbr90@gmail.com>
2017-04-17 11:06:57 +0300

    first deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-  root 'homepage#index'
-
   mount ActionCable.server => '/cable'
 
-  get 'three_hour_chart/index'
+  root 'three_hour_chart#show'
+
+
   get 'three_hour_chart/get_data', :defaults => { :format => 'json' }
   get 'three_hour_chart/show'
-  get 'homepage/show'
-
-  get 'rooms/show'
 end
