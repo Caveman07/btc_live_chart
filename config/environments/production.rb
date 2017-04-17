cc9c88482288904240eca8911c14b1b4732502e2
caveman07 <ram07kbr90@gmail.com>
2017-04-17 12:46:02 +0300

    configure websocket in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "cryptoapp_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.web_socket_server_url = "wss://calm-citadel-65024.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://calm-citadel-65024.herokuapp.com/cable', 'http://calm-citadel-65024.herokuapp.com/cable']
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
